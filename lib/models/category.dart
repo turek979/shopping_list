a76353a4905d175b9c3f0c7f3c6a181c83a94813
J.Turowski <turek.priv@gmail.com>
2024-04-23 13:41:35 +0200

    add models and basic screen

---
+++ lib/models/category.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+enum Categories {
+  vegetables,
+  fruit,
+  meat,
+  dairy,
+  carbs,
+  sweets,
+  spices,
+  convenience,
+  hygiene,
+  other
+}
+
+class Category {
+  const Category(this.categoryName, this.color);
+
+  final String categoryName;
+  final Color color;
+}

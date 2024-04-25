12406748056a42a441cc12069472139cdb356dd0
J.Turowski <turek.priv@gmail.com>
2024-04-25 09:46:45 +0200

    setup static data

--- lib/screens/groceries_screen.dart
+++ lib/screens/groceries_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list/data/dummy_items.dart';
 
 class GroceriesScreen extends StatelessWidget {
   const GroceriesScreen({super.key});
@@ -9,7 +10,29 @@ class GroceriesScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Your Groceries'),
       ),
-      body: ListView(children: [],),
+      body: ListView.builder(itemBuilder: (context, int index) {
+        return const Padding(
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              SizedBox(
+                height: 20,
+                width: 20,
+                child: DecoratedBox(
+                  decoration: BoxDecoration(color: Colors.amber),
+                ),
+              ),
+              SizedBox(width: 30),
+              Expanded(
+                child: Text(
+                  'Milk',
+                ),
+              ),
+              Text('1'),
+            ],
+          ),
+        );
+      }),
     );
   }
 }
